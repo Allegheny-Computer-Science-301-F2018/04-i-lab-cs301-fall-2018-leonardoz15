ffaffbdd4e3940ce520c2c6fe3e277356700e9a6
Zachary Leonardo <leonardoz@aldenv160.allegheny.edu>
2018-10-11 10:27:28 -0400

    On reflection

--- src/Lab4Program.r
+++ src/Lab4Program.r
@@ -26,8 +26,23 @@ dat_caliFocus$yearBlock[dat_caliFocus$year == 1950] <- "1950’s"
 dat_caliFocus$yearBlock[dat_caliFocus$year == 1960] <- "1960’s"
 dat_caliFocus$yearBlock[dat_caliFocus$year == 1970] <- "1970’s"
 ggplot(data = dat_caliFocus ) + geom_bar(mapping = aes(x = state, y = sqrt(count), fill = yearBlock), position = "dodge", stat = "identity") + theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust=-0.01))
+#OR:
+ggplot(data = dat_caliFocus ) + geom_bar(mapping = aes(x = state, y = count, fill = yearBlock), position = "dodge", stat = "identity") + theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust=-0.01))
+  The plot with the square root of the count has more variability since the bars are bigger and more spaced out.
+
 #Question 4.
+dat <- us_contagious_diseases
+dat$yearBlock[dat$year == 1950] <- "1950’s"
+dat$yearBlock[dat$year == 1960] <- "1960’s"
+dat$yearBlock[dat$year == 1970] <- "1970’s"
+ggplot(data = dat ) + geom_bar(mapping = aes(x = state, y = count, fill = yearBlock), position = "dodge", stat = "identity") + theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust=-0.01))
+  Just like the previous plot, the 1960's year block has the highest amount of contagious diseases.
 
 #Question 5.
+ggplot(data = dat ) + geom_tile(mapping = aes(x = state, y = count, color = Per100000rate), position = "dodge", stat = "identity") + theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust=-0.01))
+  This plot is still very hard to read but now the colors represent the disease rate instead of year blocks
 
 #Question 6.
+ggplot(data = asd) + geom_line(mapping = aes(x = year, y = asdrate)) + geom_vline(xintercept = 1965)
+  Using the yearly rate for Autism Spectrum Disorder in the US, provided by the WHO, I have concluded that
+that there is no increase in the autism rate after vaccines were introduced in 1965.
--- writing/reportAndReflections.md
+++ writing/reportAndReflections.md
@@ -1,8 +1,9 @@
-# Name
-# Date:
+# Name Zachary Leonardo
+# Date: Oct. 11 2018
 
 Question 7.
-
+![measles1](/src/measles1.png)
+  
 
 
 Question 8.
